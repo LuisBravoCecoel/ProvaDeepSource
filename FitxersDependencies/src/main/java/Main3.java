4cdcb6002f5cca70aeceb5d024b276ee74e91d54
lbravo <lbravo@cecoel.local>
2025-06-06 14:50:34 +0200

    prova commit en develop 5

---
+++ FitxersDependencies/src/main/java/Main3.java
@@ -0,0 +1,14 @@
+
+public class Main3 {
+
+	public String stringXXX = "";
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	private void metodo(String par1, String par2) {
+		par1 = "hola Main3";
+	}
+}
